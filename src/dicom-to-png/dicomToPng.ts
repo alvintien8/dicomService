0d7af0a5154043b62e44f791ee94b8d3f850976c
alvintien8 <alvintien357@gmail.com>
2024-06-01 01:06:22 -0400

    add separate handling for 8 bit greyscale

--- src/dicom-to-png/dicomToPng.ts
+++ src/dicom-to-png/dicomToPng.ts
@@ -6,13 +6,40 @@ import { DicomServiceError } from '../common/dicomServiceError';
 import { parseDicom, readFileData } from '../common/dicomUtils';
 import { PNG } from 'pngjs';
 
-//TODO: can make this more flexible depending on settings
-//perhaps scope to greyscale for now and tackle RGB later on.
+export const handleDicomToPng = async (ctx: Koa.Context) => {
+  try {
+    const exportPath = './data/export';
+    const fileId: string = ctx.params.fileId;
+    const outputFilePath = `${exportPath}/${fileId}.png`
+    const file = await readFileData(fileId);
+    const dataset = parseDicom(file);
+    const png = createPng(dataset);
+
+    await savePng(png, fileId);
+
+    ctx.status = 200;
+    ctx.set('Content-type', 'image/png');
+    ctx.set('Content-disposition', `attachment; filename=${fileId}.png`);
+    ctx.body = fs.createReadStream(outputFilePath);
+  } catch (err) {
+    console.error(err);
+
+    ctx.status = 500;
+    ctx.body = {
+      status: OperationStatus.ERROR,
+      errorDetails: {
+        code: err instanceof DicomServiceError ? err.code : null,
+        message: err.message
+      }
+    }
+  }
+}
+
 export const createPng = (dataset: DicomParser.DataSet) => {
   const pixelDataElement = dataset.elements.x7fe00010;
-  const pixelData = new Uint8Array(dataset.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length); //3639616
-  const height = dataset.uint16('x00280010'); //rows 1537
-  const width = dataset.uint16('x00280011'); //cols 1184
+  const pixelData = new Uint8Array(dataset.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length);
+  const height = dataset.uint16('x00280010');
+  const width = dataset.uint16('x00280011');
   const samplesPerPixel = dataset.uint16('x00280002');
   const numFrames = dataset.uint16('x00280008');
   const bitsAllocated = dataset.uint16('x00280100');
@@ -35,15 +62,25 @@ export const createPng = (dataset: DicomParser.DataSet) => {
     bgColor: { red: 0, green: 0, blue: 0 }
   });
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const pixelIndex = (y * width + x) << 1;
-      const idx = (y * width + x) << 1;
+  if (bitsStored > 8) {
+    handle16BitGreyscale(image, pixelData, bitsStored === 12);
+  } else {
+    handle8BitGreyscale(image, pixelData);
+  }
+
+  return image;
+}
+
+export const handle16BitGreyscale = (image: PNG, pixelData: Uint8Array, shouldUpscale?: boolean) => {
+  for (let y = 0; y < image.height; y++) {
+    for (let x = 0; x < image.width; x++) {
+      const pixelIndex = (y * image.width + x) << 1;
+      const idx = (y * image.width + x) << 1;
       let msb = pixelData[pixelIndex];
       let lsb = pixelData[pixelIndex + 1] & 0xFF;
 
       //handle scaling for 12 bit images
-      if (bitsStored === 12) {
+      if (shouldUpscale) {
         const ratio = (2 ** 16 - 1) / (2 ** 12 - 1)
         const upscaledValue = ((msb << 8 | lsb) * ratio) & 0xFFFF;
         msb = upscaledValue >> 8;
@@ -54,8 +91,16 @@ export const createPng = (dataset: DicomParser.DataSet) => {
       image.data[idx + 1] = lsb; // low byte
     }
   }
+}
 
-  return image;
+export const handle8BitGreyscale = (image: PNG, pixelData: Uint8Array) => {
+  for (let y = 0; y < image.height; y++) {
+    for (let x = 0; x < image.width; x++) {
+      const pixelIndex = (y * image.width + x);
+      const idx = (y * image.width + x);
+      image.data[idx] = pixelData[pixelIndex];
+    }
+  }
 }
 
 export const savePng = async (png: PNG, fileId: string) => {
@@ -69,31 +114,4 @@ export const savePng = async (png: PNG, fileId: string) => {
   await new Promise((res, rej) => {
     png.pack().pipe(fs.createWriteStream(`${exportPath}/${fileId}.png`, { flags: 'w' })).on('finish', res).on('error', rej);
   })
-}
-
-export const handleDicomToPng = async (ctx: Koa.Context) => {
-  try {
-    const exportPath = './data/export';
-    const fileId: string = ctx.params.fileId;
-    const outputFilePath = `${exportPath}/${fileId}.png`
-    const file = await readFileData(fileId);
-    const dataset = parseDicom(file);
-    const png = createPng(dataset);
-
-    await savePng(png, fileId);
-
-    ctx.status = 200;
-    ctx.set('Content-type', 'image/png');
-    ctx.set('Content-disposition', `attachment; filename=${fileId}.png`);
-    ctx.body = fs.createReadStream(outputFilePath);
-  } catch (err) {
-    ctx.status = 500;
-    ctx.body = {
-      status: OperationStatus.ERROR,
-      errorDetails: {
-        code: err instanceof DicomServiceError ? err.code : null,
-        message: err.message
-      }
-    }
-  }
 }
